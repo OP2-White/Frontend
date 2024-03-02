0e40b36024e3edfb439989d5e24fa5088e81e326
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-03-02 16:55:46 +0200

    Created react router navigtion and a navigation bar with links to each page.

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {RouterProvider, createBrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
-import CreateProduct from './components/CreateProduct.jsx';
-import './index.css'
-import CalorieCalculator from './components/kalorilaskuri.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import CreateProduct from "./components/CreateProduct.jsx";
+import "./index.css";
+import CalorieCalculator from "./components/kalorilaskuri.jsx";
+import SearchProducts from "./components/SearchProducts.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        element: <SearchProducts />,
+        index: true,
+      },
+      {
+        path: "search-products",
+        element: <SearchProducts />,
+      },
+      {
+        path: "create-product",
+        element: <CreateProduct />,
+      },
+      {
+        path: "calorieconsumption",
+        element: <CalorieCalculator />,
+      },
+    ],
   },
-  {
-    path: "/create-product",
-    element: <CreateProduct />,
-    },
-    {
-      path: "/calorieconsumption",
-      element: <CalorieCalculator />,
-    }
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <RouterProvider router={router} />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
-
