0866bef9c5906e27aeff42fe05acaa84bc9f906c
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-03-12 08:13:10 +0200

    Changed fetch urls

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -2,35 +2,19 @@ import { useEffect, useState } from "react";
 
 function CreateProduct() {
   const [newProduct, setNewProduct] = useState({
-    name: "",
+    foodName: "",
     calories: 0,
-    serving_size_g: 0,
-    fat_total_g: 0,
-    fat_saturated_g: 0,
-    protein_g: 0,
-    sodium_mg: 0,
-    potassium_mg: 0,
-    cholesterol_mg: 0,
-    carbohydrates_total_g: 0,
-    fiber_g: 0,
-    sugar_g: 0,
+    protein: 0,
+    carbs: 0,
+    fat: 0,
   });
 
-  const [list, setList] = useState([]);
-
   const fields = [
-    { name: "name", label: "Name" },
+    { name: "foodName", label: "Name" },
     { name: "calories", label: "Calories" },
-    { name: "serving_size_g", label: "Serving Size (g)" },
-    { name: "fat_total_g", label: "Total Fat (g)" },
-    { name: "fat_saturated_g", label: "Saturated Fat (g)" },
-    { name: "protein_g", label: "Protein (g)" },
-    { name: "sodium_mg", label: "Sodium (mg)" },
-    { name: "potassium_mg", label: "Potassium (mg)" },
-    { name: "cholesterol_mg", label: "Cholesterol (mg)" },
-    { name: "carbohydrates_total_g", label: "Total Carbohydrates (g)" },
-    { name: "fiber_g", label: "Fiber (g)" },
-    { name: "sugar_g", label: "Sugar (g)" },
+    { name: "protein", label: "protein" },
+    { name: "carbs", label: "carbs" },
+    { name: "fat", label: "fat" },
   ];
 
   const renderFields = () => {
@@ -57,51 +41,35 @@ function CreateProduct() {
       [name]: value,
     }));
   };
+  // pitää kattoo heroku postgre sql juttuja. ei toimi ku tulee error 500
+  const saveToDatabase = async () => {
+    try {
+      const response = await fetch(
+        "https://calorie-calculator-backend-c99d1a21f171.herokuapp.com/saveFoodREST",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newProduct),
+        }
+      );
 
-  const addProduct = () => {
-    setList((prevList) => [...prevList, newProduct]);
-    setNewProduct({
-      name: "",
-      calories: 0,
-      serving_size_g: 0,
-      fat_total_g: 0,
-      fat_saturated_g: 0,
-      protein_g: 0,
-      sodium_mg: 0,
-      potassium_mg: 0,
-      cholesterol_mg: 0,
-      carbohydrates_total_g: 0,
-      fiber_g: 0,
-      sugar_g: 0,
-    });
-  };
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
 
-  useEffect(() => {
-    console.log(list);
-  }, [list]);
-
-  const saveToDatabase = () => {
-    fetch("http://localhost:8008/foodlistREST", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(searchResult),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Tallennus onnistui:", data);
-      })
-      .catch((error) => {
-        console.error("Tallennusvirhe:", error);
-      });
+      console.log("Data saved successfully");
+    } catch (error) {
+      console.error("Tallennusvirhe:", error.message);
+    }
   };
 
   return (
     <div className="createProductContainer">
       <p>Create new product:</p>
       <form>{renderFields()}</form>
-      <button onClick={saveToDatabase}>Save</button>
+      <button onClick={() => saveToDatabase()}>Save</button>
     </div>
   );
 }
